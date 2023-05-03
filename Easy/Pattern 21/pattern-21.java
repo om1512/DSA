dc1e0bc474014e9bd7df4d26396161660b08e07b
Om Dhingani <72222606+om1512@users.noreply.github.com>
2023-05-03 16:52:17 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Pattern 21/pattern-21.java
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Main {
+    // Driver code
+    public static void main(String[] args) throws Exception {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            Solution obj = new Solution();
+            obj.printSquare(n);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+
+    void printSquare(int n) {
+        // code here
+        for(int i=0;i<n;i++){
+            if(i == 0  || i == n-1){
+                for(int j=0;j<n;j++){
+                    System.out.print("*");
+                }
+            }else{
+                System.out.print("*");
+                for(int j=0;j<n-2;j++) {
+                    System.out.print(" ");
+                }  
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
