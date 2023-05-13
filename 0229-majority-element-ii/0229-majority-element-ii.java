42f71553e16800f8aae76f1e7c441f12d14158cb
Om Dhingani <72222606+om1512@users.noreply.github.com>
2023-05-13 12:34:51 +0530

    Time: 19 ms (6.3%), Space: 45.7 MB (85.43%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -3,13 +3,11 @@ class Solution {
         Map<Integer,Integer> m = new HashMap<>();
         List<Integer> l = new ArrayList<>();
         int n = nums.length;
+        int min = n/3;
         for(int i=0;i<n;i++){
             m.put(nums[i],m.getOrDefault(nums[i],0)+1);
-        }
-        int min = n/3;
-        for(Map.Entry<Integer,Integer> e : m.entrySet()){
-            if(e.getValue() > min){
-                l.add(e.getKey());
+            if(m.getOrDefault(nums[i],0) > min && !l.contains(nums[i])){
+                l.add(nums[i]);
             }
         }
 
