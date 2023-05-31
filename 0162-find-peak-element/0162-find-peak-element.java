c9bac3fdec7feff5deb33d95861dc830d761b6e1
Om Dhingani <72222606+om1512@users.noreply.github.com>
2023-05-31 16:16:11 +0530

    Time: 0 ms (100%), Space: 41.6 MB (86.3%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,23 +1,16 @@
 class Solution {
-    public int findPeakElement(int[] nums) {
-        int n = nums.length,left = 0, right = n-1;
-        while(left < right){
-            int mid = (left + right)/2;
-            if(mid == 0){
-                return nums[0] >= nums[1] ? 0 : 1;
-            }
-            else if(mid == n-1){
-                return nums[n-1] >= nums[n-2] ? n-1 : n-2;
-            }
-            else if(nums[mid] >= nums[mid-1] && nums[mid] >= nums[mid+1]){
-                return mid;
-            }
-            else if(nums[mid] < nums[mid-1]){
-                right = mid-1;
-            }else{
-                left = mid+1;
-            }
+    public static int peekFinder(int[] nums,int low,int high,int n){
+        int mid = (low+high)/2;
+        if((mid == 0 || nums[mid-1]<=nums[mid]) && (mid == n-1 || nums[mid+1] <= nums[mid])){
+            return mid;
+        }else if(mid > 0 && nums[mid-1]>nums[mid]){
+            return peekFinder(nums,low,mid-1,n);
+        }else{
+            return peekFinder(nums,mid+1,high,n);
         }
-        return left;
+    }
+    public int findPeakElement(int[] nums) {
+        int n = peekFinder(nums,0,nums.length-1,nums.length);
+        return n;
     }
 }
