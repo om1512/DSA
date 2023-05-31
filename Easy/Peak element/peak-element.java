e46159f9ba8492ae43b420ba1b3d8cf0380d1b1e
Om Dhingani <72222606+om1512@users.noreply.github.com>
2023-05-31 15:40:49 +0530

    Updated Solution - GfG to GitHub

--- Easy/Peak element/peak-element.java
+++ Easy/Peak element/peak-element.java
@@ -49,26 +49,34 @@ class Solution
 	// Function to find the peak element
 	// arr[]: input array
 	// n: size of array a[]
-	public int peakElement(int[] nums,int n)
+	public int peakElement(int[] arr,int n)
     {
-        int left = 0, right = n-1;
-        while(left < right){
-            int mid = (left + right)/2;
-            if(mid == 0){
-                return nums[0] >= nums[1] ? 0 : 1;
-            }
-            else if(mid == n-1){
-                return nums[n-1] >= nums[n-2] ? n-1 : n-2;
-            }
-            else if(nums[mid] >= nums[mid-1] && nums[mid] >= nums[mid+1]){
-                return mid;
-            }
-            else if(nums[mid] < nums[mid-1]){
-                right = mid-1;
-            }else{
-                left = mid+1;
-            }
+       int peekNum = Integer.MIN_VALUE,index=0;
+       
+       if(n>1){
+           if(arr[0]>arr[1]){
+               if(peekNum <= arr[0]){
+                   peekNum = arr[0];
+                   index = 0;
+               }
+           }
+           if(arr[n-2]<=arr[n-1]){
+               if(peekNum < arr[n-1]){
+                   peekNum = arr[n-1];
+                   index = n-1;
+               }
+           }
+       }else{
+           return 0;
+       }
+        for(int i=1;i<n-1;i++){
+               if(arr[i]>=arr[i-1] && arr[i]>=arr[i+1]){
+                   if(peekNum < arr[i]){
+                       peekNum = arr[i];
+                       index = i;
+                   }
+               }
         }
-        return left;
+       return index;
     }
 }
