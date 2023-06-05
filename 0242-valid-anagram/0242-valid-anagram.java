e5aec0260669994f77d49d24eae90df5d8a6fb99
Om Dhingani <72222606+om1512@users.noreply.github.com>
2023-06-05 11:22:12 +0530

    Time: 4 ms (80.5%), Space: 43.8 MB (18.07%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,23 +3,11 @@ class Solution {
         if(s.length() != t.length()){
             return false;
         }
-        HashMap<Character,Integer> m = new HashMap<>();
-        for(int i=0;i<s.length();i++){
-        m.put(s.charAt(i), m.getOrDefault(s.charAt(i), 0)+1);
-        }
-        for(int i=0;i<s.length();i++){
-        if(m.containsKey(t.charAt(i))){
-            m.put(t.charAt(i),m.get(t.charAt(i))-1);
-        }else{
-            return false;
-        }
-        }
-        Set<Character> keys = m.keySet();
-        for (Character key : keys) {
-            if (m.get(key) != 0) {
-                return false;
-            }
-        }
-        return true;
+        char[] c1 = s.toCharArray();
+        char[] c2 = t.toCharArray();
+        Arrays.sort(c1);
+        Arrays.sort(c2);
+        if(Arrays.equals(c1,c2)) return true;
+        return false;
     }
 }
