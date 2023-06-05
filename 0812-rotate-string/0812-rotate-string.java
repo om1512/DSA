b281e004d242bed59ad3daa32e78020977082a4a
Om Dhingani <72222606+om1512@users.noreply.github.com>
2023-06-05 11:02:50 +0530

    Time: 0 ms (100%), Space: 40.1 MB (98.8%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.java
+++ 0812-rotate-string/0812-rotate-string.java
@@ -1,16 +1,8 @@
 class Solution {
-    static String rightRotation(String s){
-      return s.substring(1) + s.substring(0,1);
-    }
     public boolean rotateString(String str, String str2) {
-        int count = 0;
-        while(count<str.length()){
-            str = rightRotation(str);
-            if(str.equals(str2)){
-                return true;
-            }
-            count++;
-        }   
+        if(str.length() != str2.length()) return false;
+        String temp = str+str;
+        if(temp.indexOf(str2) != -1) return true;
         return false;
     }
 }
