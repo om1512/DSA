ac74ccdf462fe3e191e6494d8ed9ac8e8228bcc1
Om Dhingani <72222606+om1512@users.noreply.github.com>
2023-08-18 23:49:37 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Move all negative elements to end/move-all-negative-elements-to-end.java
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+            PrintWriter ot = new  PrintWriter(System.out);
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            int a[] = new int[(int)(n)];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(inputLine[i]);
+            }
+            
+            Solution obj = new Solution();
+            obj.segregateElements(a, n);
+            
+            for(int i=0;i<n;i++)
+            ot.print(a[i]+" ");
+            
+            ot.println();
+        }
+        ot.close();
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    
+    public void segregateElements(int arr[], int n)
+    {
+        // Your code goes here
+        ArrayList<Integer> positive = new ArrayList<>();
+        ArrayList<Integer> negetive = new ArrayList<>();
+        for(int i:arr){
+            if(i >= 0) positive.add(i);
+            else negetive.add(i);
+        }
+        int i = 0;
+        for(i=0;i<positive.size();i++){
+            arr[i] = positive.get(i);
+        }
+        for(int j=0;j<negetive.size();j++){
+            arr[i++] = negetive.get(j);
+        }
+    }
+}
