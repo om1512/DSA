93d4bf437c1588004d2179ee2648392ea82605d1
Om Dhingani <72222606+om1512@users.noreply.github.com>
2024-05-05 12:26:20 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Top View of Binary Tree/top-view-of-binary-tree.java
@@ -0,0 +1,166 @@
+//{ Driver Code Starts
+//Initial Template for JAVA
+
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+public class Tree {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    static void printInorder(Node root)
+    {
+        if(root == null)
+            return;
+            
+        printInorder(root.left);
+        System.out.print(root.data+" ");
+        
+        printInorder(root.right);
+    }
+    
+	public static void main (String[] args) throws IOException{
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t > 0){
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+    	    	
+                Solution ob = new Solution();
+
+                ArrayList<Integer> vec = ob.topView(root);
+                for(int x : vec)
+                    System.out.print(x + " ");
+                System.out.println();
+        	
+                t--;   
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+*/
+
+class Solution
+{
+    static class Block{
+        int depth;
+        int val;
+        Block(){}
+        Block(int depth, int val){
+            this.depth = depth;
+            this.val = val;
+        }
+    }
+    static TreeMap<Integer, Block> solve(Node root, int vertical, int depth, TreeMap<Integer, Block> map){
+        if(root == null) return null;
+        if(map.containsKey(vertical)){
+            Block b = map.get(vertical);
+            if(depth < b.depth){
+                b.val = root.data;
+                b.depth = depth;
+                map.put(vertical, b);
+            }
+        }else{
+            Block b = new Block();
+            b.depth = depth;
+            b.val = root.data;
+            map.put(vertical, b);
+        }
+        solve(root.left, vertical-1, depth+1, map);
+        solve(root.right, vertical+1, depth+1, map);
+        return map;
+    } 
+    static ArrayList<Integer> topView(Node root)
+    {
+        TreeMap<Integer, Block> map = solve(root, 0,0,new TreeMap<>());
+        ArrayList<Integer> list = new ArrayList<>();
+        map.forEach((key, value) -> {
+            list.add(value.val);
+        });
+        return list;
+    }
+}
