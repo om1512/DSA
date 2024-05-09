ea176d321e7002371ab18b3c3686397224fb1af9
Om Dhingani <72222606+om1512@users.noreply.github.com>
2024-05-09 15:40:37 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Introduction to DP/introduction-to-dp.java
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.math.*;
+import java.io.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int n = sc.nextInt();
+            Solution obj = new Solution();
+            long topDownans = obj.topDown(n);
+            long bottomUpans = obj.bottomUp(n);
+            if (topDownans != bottomUpans)
+                System.out.println(-1);
+            else
+                System.out.println(topDownans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+
+    static long topDown(int n) {
+        // code here
+        long dp[]=new long[n+1];
+        Arrays.fill(dp,-1);
+        return memo(n,dp);
+    }
+    static long mod=1000000007;
+    static long memo(int n, long[] dp)
+    {
+        if(n<=1)    return n;
+        if(dp[n]!=-1)   return dp[n];
+        else
+            return dp[n]=(memo(n-1,dp)+memo(n-2,dp))%mod;
+    }
+    
+
+    static long bottomUp(int n) {
+        // code here
+        long mod=1000000007;
+        long dp[]=new long[n+1];
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<=n;i++)
+        {
+            dp[i]=(dp[i-1]+dp[i-2])%mod;
+        }
+        return dp[n];
+    }
+}
