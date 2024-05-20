d25b87790b028ddf01ecf9adab2268919ec9e702
Om Dhingani <72222606+om1512@users.noreply.github.com>
2024-05-20 14:43:32 +0530

    Updated Solution - GfG to GitHub

--- Easy/Bubble Sort/bubble-sort.java
+++ Easy/Bubble Sort/bubble-sort.java
@@ -15,8 +15,8 @@ class Solution
     {
         //code here
         
-        for(int i=0;i<n-1;i++){
-            for(int j=0;j<n-i-1;j++){
+        for(int i=n-1;i>0;i--){
+            for(int j=0;j<i;j++){
                 if(arr[j] > arr[j+1]){
                 int temp = arr[j+1];
                 arr[j+1] = arr[j];
@@ -26,6 +26,7 @@ class Solution
         }
     }
 }
+
 
 //{ Driver Code Starts.
 
