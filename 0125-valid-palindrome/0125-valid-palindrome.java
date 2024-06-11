df48a351955051267c92bb92c21b57113554cf25
Om Dhingani <72222606+om1512@users.noreply.github.com>
2024-06-11 17:43:04 +0530

    Time: 3 ms (72.89%), Space: 42.9 MB (64.29%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.trim().toLowerCase();
+        int i = 0, j = s.length() - 1;
+        while(i <= j){
+            if(!Character.isLetterOrDigit(s.charAt(i))) i++;
+            else if(!Character.isLetterOrDigit(s.charAt(j))) j--;
+            else if(s.charAt(i) != s.charAt(j)){
+                return false;
+            }else{
+                i++;
+                j--;
+            }
+        }
+        return true;
+    }
+}
